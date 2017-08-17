1d00db1578d4721e36e406bf8425aa25b28493bf
unknown <>
2017-08-18 02:15:26 +0800

    update

--- pixiv.py
+++ pixiv.py
@@ -21,14 +21,16 @@ def search(info):
     print(data)
     return render_template('result.html', title = data['title'], auth = data['auth'],
                            href = data['href'], src = data['src'], page_list = data['page_list'],
-                           current_page = data['current_page'], length = data['length'])
+                           current_page = data['current_page'], length = data['length'],
+                           auth_href = data['auth_href'], ID = data['ID'])
 
-##@app.route('/image/<info>', methods = ['GET'])
-##def search(info):
-##    data = json.loads(info)
-##    print(data)
-##    return render_template('image.html', image_title = data['title'], image_auth = data['auth'],
-##                           image_src = data['src'])
+@app.route('/image/<info>', methods = ['GET'])
+def imgae(info):
+    info = json.loads(info)
+    data = ImageBuilder(info['ID'])
+    print(data)
+    return render_template('image.html', image_src = info['save_src'], 
+                           icon = data['icon'], date = data['date'], user_href = data['href'])
 
 if __name__ == '__main__':
     app.run(debug = True)
--- stream.py
+++ stream.py
@@ -3,14 +3,17 @@ import requests, re, os
 from urllib.parse import quote
 
 def StreamUpdate(key, page = 1):
+    work_path = os.getcwd()
     print(key)
     URLs = "".join(['https://www.pixiv.net/search.php?word=', key, '&p=', str(page)])
     headers = {'Referer' : 'https://www.pixiv.net/',
                'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'
                }
-    r = requests.Session()
     
-    soup = BeautifulSoup(r.get(URLs, headers = headers).content, "lxml")
+    for i in range(10):
+        print('retry : ' + str(i))
+        soup = BeautifulSoup(requests.get(URLs, headers = headers).content, "lxml")
+        if soup.find('ul', '_image-items autopagerize_page_element'): break
     print(soup.prettify())
     body = soup.find('ul', '_image-items autopagerize_page_element')
 
@@ -18,42 +21,53 @@ def StreamUpdate(key, page = 1):
                'Referer' : 'https://www.pixiv.net/search.php?word=' + quote(key)}
 
     image_info = body.find_all('li', 'image-item')
-    page_num = len(soup.find('div', 'pager-container').ul.find_all('li')) - 2
-    href= []; raw_img = []; title = []; auth = [];
 
-    os.chdir(os.path.abspath('/PixivApp/static'))
-    if not os.path.exists('cache'):
-        os.mkdir('cache')
-    os.chdir('cache')
+    if soup.find('ul', 'page-list'):
+        page_num = len(soup.find('ul', 'page-list').find_all('li')) - 2
+    else:
+        page_num = 1
     
+    href= []; raw_img = []; title = []; auth = []; auth_href = []; ID = [];
+    
+    if not os.path.exists(os.path.join(os.getcwd(), 'static', 'cache')):
+        os.mkdir(os.path.join(os.getcwd(), 'static', 'cache'))
+    os.chdir(os.path.join(os.getcwd(), 'static', 'cache'))
+    
+    r = requests.Session()
     for i in image_info:
         title.append(i.find('h1', 'title').string)
         auth.append(i.find('a', 'user ui-profile-popup').string)
+        auth_href.append("")
+        ID.append(i.find('a', 'user ui-profile-popup')['href'].split('=')[-1])
         href.append(i.a['href'])
         img_rp = r.get("".join(['https://www.pixiv.net/', i.a['href']]), headers = info_headers)
-        src = BeautifulSoup(img_rp.content, "lxml").find('div','img-container').img['src']
-        save_src = src.split('/img/')[1].replace('/', '-').strip()
+        img_url = BeautifulSoup(img_rp.content, "lxml").find('div','img-container').img['src']
+        print(img_url)
         
+        save_src = img_url.split('/img/')[1].replace('/', '-').strip()
+
         if not os.path.exists(save_src):
             with open(save_src, 'wb') as f:
-                f.write(r.get(src, headers = headers).content)
+                f.write(r.get(img_url, headers = headers).content)
         
         raw_img.append(save_src)
 
     data = {
             'src' : raw_img, 'href' : href, 'auth' : auth, 'title' : title,
             'page_list' : [i for i in range(page, page_num + page)], 'current_page' : page,
-            'length' : len(raw_img)
+            'length' : len(raw_img), 'auth_href' : auth_href, 'ID' : ID 
             }
+
+    os.chdir(work_path)
     return data
 
 def ImageBuilder(ID):
-    URL = 'https://www.pixiv.net/member_illust.php?mode=medium&illust_id=' + ID
-    r = requests.Session()
+    print(ID)
+    URL = 'https://www.pixiv.net/member_illust.php?mode=medium&illust_id=' + ID 
     headers = {'Referer' : 'https://www.pixiv.net/',
                'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'
                }
-    soup = BeautifulSoup(r.get(URL, headers = headers).content, "lxml")
+    soup = BeautifulSoup(requests.get(URL, headers = headers).content, "lxml")
 
     body = soup.find('div', 'cool-work-main')
     titlearea = body.find('div', 'titlearea')
@@ -61,13 +75,8 @@ def ImageBuilder(ID):
     user = {
             'icon' : titlearea.find('div', 'usericon').img['src'],
             'href' : titlearea.find('div', 'usericon').a['href'],
-            'name' : titlearea.find('div', 'userdata').h2.a.string,
+            'auth' : titlearea.find('div', 'userdata').h2.a.string,
             'date' : titlearea.find('div', 'userdata').span.string
             }
 
     return user
-
-    
-
-
-    
